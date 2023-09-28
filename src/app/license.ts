54f26b37e11784716e35c9a7886a8f2dd3ed41cc
Maren Süwer <maren.suewer@cloudogu.com>
2023-09-28 13:13:22 +0200

    added portainer license

--- src/app/license.ts
+++ src/app/license.ts
@@ -1566,6 +1566,25 @@ export const LICENSES: License[] = [
             OR OTHER DEALINGS IN THE SOFTWARE.`
     },
     {
+      name: "Portainer",
+      description: `
+      <p>Copyright (c) 2018 Portainer.io</p>
+      
+      <p>This software is provided 'as-is', without any express or implied
+      warranty.  In no event will the authors be held liable for any damages
+      arising from the use of this software.</p>
+      <p>Permission is granted to anyone to use this software for any purpose,
+      including commercial applications, and to alter it and redistribute it
+      freely, subject to the following restrictions:</p>
+      <p>1. The origin of this software must not be misrepresented; you must not
+         claim that you wrote the original software. If you use this software
+         in a product, an acknowledgment in the product documentation would be
+         appreciated but is not required.</p>
+         <p>2. Altered source versions must be plainly marked as such, and must not be
+         misrepresented as being the original software.</p>
+         <p>3. This notice may not be removed or altered from any source distribution.`
+  },
+    {
         name: "Atlassian Jira",
         description: `<p>Copyright (C) 2023 Cloudogu GmbH</p>
 
